c6c1537e66d9f0f8e852f3aed88de83a1ab8b9bc
lavsinner <www.alex.lisovoy@gmail.com>
2017-01-15 14:55:28 +0200

    Added mail configuration

--- config/web.php
+++ config/web.php
@@ -23,11 +23,16 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-            'fileTransportPath' => '@runtime/mail'
+            'useFileTransport' => false,//set this property to false to send mails to real email addresses
+            //comment the following array to send mail using php's mail function
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'your_mail',
+                'password' => 'your_specific_app_pass',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
